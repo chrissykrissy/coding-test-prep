a832baa23e4a8f27845599a103bbc2bec96e3c91
chrissykrissy <jj2174@nyu.edu>
2024-10-05 12:54:20 +0900

    [Gold II] Title: 가운데를 말해요, Time: 476 ms, Memory: 40248 KB -BaekjoonHub

---
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int num = Integer.parseInt(st.nextToken());
+        int[] list = new int[num];
+
+        for (int i = 0; i < num; i++) {
+            st = new StringTokenizer(br.readLine());
+            list[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Queue<Integer> max = new PriorityQueue<>((a,b) -> b-a);
+        Queue<Integer> min = new PriorityQueue<>();
+
+        for (int i = 0; i < num; i++) {
+             max.offer(list[i]);
+             min.offer(max.poll());
+            if (max.size() < min.size()) {
+                max.offer(min.poll());
+            }
+
+            if(max.size() > min.size()) bw.write(String.valueOf(max.peek()));
+            else{
+                bw.write(String.valueOf(Math.min(max.peek(), min.peek())));
+            }
+            bw.write("\n");
+        }
+        bw.flush();
+    }
+}
