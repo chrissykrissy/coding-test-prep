6bf822a1fdf46a5b2eca664159850bd010dc204d
chrissykrissy <jj2174@nyu.edu>
2024-10-10 13:02:45 +0900

    [level 3] Title: 야근 지수, Time: 126.35 ms, Memory: 68 MB -BaekjoonHub

---
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.java
@@ -0,0 +1,27 @@
+import java.util.PriorityQueue;
+class Solution {
+    public long solution(int n, int[] works) {
+        //피로도 = 야근 시작한 시점에서 남은 일의 작업량을 제곱하여 더한 값
+        //n = 퇴근까지 남은 시간
+        //최소화하려면 제곱하는 숫자가 적어야함
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> b-a);
+        for (int i = 0; i < works.length; i++) {
+            pq.add(works[i]);
+        }
+        while(n > 0 && !pq.isEmpty()){
+            int max = pq.poll();
+            max--;
+            if(max > 0){
+                pq.add(max);
+            }
+            n--;
+        }
+        
+        long answer = 0;
+        while(!pq.isEmpty()){
+            int top = pq.poll();
+            answer += top*top;
+        }
+        return answer;
+    }
+}
