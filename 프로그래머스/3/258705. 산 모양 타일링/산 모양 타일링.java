1c24308d20dac79422deaac6c8d644d1a510ea70
chrissykrissy <jj2174@nyu.edu>
2024-10-12 16:03:24 +0900

    [level 3] Title: 산 모양 타일링, Time: 4.37 ms, Memory: 90 MB -BaekjoonHub

---
+++ 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n, int[] tops) {
+        int[] a = new int[n];
+        int[] b = new int[n];
+        a[0] = 1;
+        b[0] = 2 + tops[0];
+        for(int i = 1; i < n; i++){
+            a[i] = (a[i-1]+b[i-1])%10007;
+            b[i] = ((1+tops[i]) * a[i-1] + (2 + tops[i]) * b[i-1]) % 10007;
+        }
+        return (a[n-1]+b[n-1])%10007;
+    }
+}
