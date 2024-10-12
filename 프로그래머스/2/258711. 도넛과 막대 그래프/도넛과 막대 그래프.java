9cdafb9cfd8b8d271c95b6a4d5dbf4b7ad44fd71
chrissykrissy <jj2174@nyu.edu>
2024-10-12 14:38:43 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 359.11 ms, Memory: 284 MB -BaekjoonHub

---
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
@@ -0,0 +1,53 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[][] edges) {
+        int[] answer = new int[4];
+        //정점번호, 도넛모양수, 막대모양수, 8자모양수
+        /*
+        1. 각 노드별,
+        경우 1) out == 0 : 직선 그래프 갯수 + 1
+        경우 2) out == 1 : 흔한 노드.. 무시
+        경우 3) out == 2 :
+                    if in > 0 : 8자 그래프 갯수 += 1
+                    else : 이 노드가 정점
+        경우 4) out > 2 : 이 노드가 정점
+        도넛갯수 = 정점 out - 막대 - 8자
+         */
+        //key is id, v = [inbound, outbound]
+        Map<Integer, int[]> graph = new HashMap<>();
+
+        for(int[] e : edges){
+            int from = e[0];
+            int to = e[1];
+            if(!graph.containsKey(from)){
+                graph.put(from, new int[2]);
+            }
+            if(!graph.containsKey(to)){
+                graph.put(to, new int[2]);
+            }
+            graph.get(from)[1]++;
+            graph.get(to)[0]++;
+        }
+
+        for(Integer n : graph.keySet()){
+            if(graph.get(n)[1] == 0){
+                answer[2]++;
+            }
+            else if(graph.get(n)[1] == 1) {
+                continue;
+            }
+            else if(graph.get(n)[1] == 2){
+                if(graph.get(n)[0] > 0){
+                    answer[3]++;
+                }else{
+                    answer[0] = n;
+                }
+            }else{
+                answer[0] = n;
+            }
+        }
+        answer[1] = graph.get(answer[0])[1]-answer[2]-answer[3];
+
+        return answer;
+    }
+}
